1294c0e86b4f525101d364d7a634d4134197cedf
Sreehari B S <sreeharibs95@gmail.com>
2022-04-15 09:26:25 +0530

    deploy script update

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 sudo kill -9 $(sudo lsof -t -i:443)
-sudo ojet build --release && sudo nohup ojet serve --server-port=443 --release &
+sudo ojet build --release && sudo nohup ojet serve web --server-port=443 --release &
